725d738a1f29a4b729922fcff33abcdc6f0a002d
Neo (Donggeun) <dg.chi@fitpet.co.kr>
2020-06-13 22:35:39 +0900

    Add frontend

---
+++ frontend/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import {CssBaseline} from '@material-ui/core';
+import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <CssBaseline/>
+    <App/>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+serviceWorker.unregister();
